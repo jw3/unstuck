47dd7cff135116118ac534729763517e7a7db07c
John Wass <jwass3@gmail.com>
2021-07-29 06:43:53 -0400

    add clap

--- README.md
+++ README.md
@@ -5,12 +5,24 @@ For fixing a Kubernetes namespace stuck in terminating state.
 
 https://www.openshift.com/blog/the-hidden-dangers-of-terminating-namespaces
 
-### Two approaches
-1. [Right](https://github.com/kubernetes/kubernetes/issues/60807#issuecomment-524772920)
-2. [YOLO](https://github.com/kubernetes/kubernetes/issues/60807#issuecomment-408599873)
+### Options
+1. [Fix](https://github.com/kubernetes/kubernetes/issues/60807#issuecomment-524772920)
+2. [Force](https://github.com/kubernetes/kubernetes/issues/60807#issuecomment-408599873)
 
 ### Usage
-`unstuck <namespace>`
+```plain
+unstuck 0.0.0
+Unstick a stuck namespace
+
+USAGE:
+    unstuck <namespace> <action>
+
+ARGS:
+    <namespace>    The namespace
+    <action>       The action to take (find, fix, force)
+```
+
+
 
 ## Warranty
 None
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,12 +1,45 @@
-use unstuck::{api, yolo};
+use clap::Clap;
+
+use unstuck::{api, find, fix, yolo};
+use std::str::FromStr;
+
+#[derive(Clap)]
+#[clap(name = "unstuck", version="0.0.0")]
+/// Unstick a stuck namespace
+struct Opts {
+    /// The namespace
+    namespace: String,
+
+    /// The action to take (find, fix, force)
+    action: Op,
+}
+
+#[derive(Clap)]
+enum Op {
+    Find,
+    Fix,
+    Force,
+}
+
+impl FromStr for Op {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "find" => Ok(Op::Find),
+            "fix" => Ok(Op::Fix),
+            "force" => Ok(Op::Force),
+            _ => Err("no match"),
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), api::Error> {
-    let args: Vec<String> = std::env::args().collect();
-    match args.get(1) {
-        None => println!("usage: unstuck <namespace>"),
-        Some(ns) => yolo::brute_force_fix(&ns).await?,
+    let opts: Opts = Opts::parse();
+    match opts.action {
+        Op::Find => find::whats_stuck(&opts.namespace).await,
+        Op::Fix => fix::surgical_strike(&opts.namespace).await,
+        Op::Force => yolo::brute_force_fix(&opts.namespace).await,
     }
-
-    Ok(())
 }
